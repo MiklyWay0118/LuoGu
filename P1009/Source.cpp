266c3965f198e030c0ef50cfbf4068d18578c319
YingHe01 <188996@qq.com>
2024-04-14 21:55:21 +0800

    0414_2

--- P1009/Source.cpp
+++ P1009/Source.cpp
@@ -1,17 +1,19 @@
 // Ã‚‘¥: https://www.luogu.com.cn/problem/P1009
 #include <stdio.h>
-#include <math.h>
-#include <vector>
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
 	int n = 0;
-	vector <int>errf;
-	for (int i = 0; i < 2; i++)
+	cin >> n;
+	long long muns = 1;
+	for (int i = 1; i <= n; i++)
 	{
-		
+		muns = muns * i;
 	}
+	cout << muns;
 
+	return 0;
 }
